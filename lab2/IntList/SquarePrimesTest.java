1f424871caa9d4ac42b713a51cc0cf088d0a003b
gg <1229244193@qq.com>
2023-08-21 16:55:43 +0800

    accomplish lab2 first1.1

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -12,9 +12,12 @@ public class SquarePrimesTest {
      */
     @Test
     public void testSquarePrimesSimple() {
-        IntList lst = IntList.of(14, 15, 16, 17, 4);
+//        IntList lst = IntList.of(3, 15, 16, 17, 4);
+        IntList lst = IntList.of(13, 4);
         boolean changed = IntListExercises.squarePrimes(lst);
-        assertEquals("14 -> 15 -> 16 -> 289 -> 4", lst.toString());
+//        assertEquals("9 -> 15 -> 16 -> 289 -> 4", lst.toString());
+        assertEquals("169 -> 4", lst.toString());
         assertTrue(changed);
+//        assertFalse(changed);
     }
 }
