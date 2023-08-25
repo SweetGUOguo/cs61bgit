19c507f587b4d644e082d2af60ef9dca228b0232
gg <1229244193@qq.com>
2023-08-25 19:58:52 +0800

    finish proj1 checkpoint

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,135 @@
+package deque;
+
+public class LinkedListDeque<Item> {
+    private StuffNode sentinel;
+    private int size;
+
+    /**
+     * Creates an empty linked list deque.
+     */
+    public LinkedListDeque() {
+        sentinel = new StuffNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+//        sentinel = new StuffNode(null, sentinel, sentinel);
+        size = 0;
+    }
+
+    public LinkedListDeque(Item i) {
+        sentinel = new StuffNode(null, null, null);
+        sentinel.next = new StuffNode(i, sentinel, sentinel);
+        size = 1;
+    }
+
+    /**
+     * Adds x to the front of the list.
+     */
+    public void addFirst(Item i) {
+        sentinel.next.prev = new StuffNode(i, sentinel, sentinel.next);
+        sentinel.next = sentinel.next.prev;
+        size = size + 1;
+    }
+
+    /**
+     * Adds x to the end of the list.
+     */
+    public void addLast(Item i) {
+        sentinel.prev.next = new StuffNode(i, sentinel.prev, sentinel);
+        sentinel.prev = sentinel.prev.next;
+        size = size + 1;
+    }
+
+    /**
+     * Judge if the deque is empty.
+     */
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the number of items in the deque.
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Prints the items in the deque from first to last.
+     */
+    public void printDeque() {
+        if (!isEmpty()) {
+            StuffNode nowprint = sentinel.next;
+            for (int i = 0; i < size; i++) {
+                System.out.print(nowprint.item);
+                nowprint = nowprint.next;
+                System.out.print(' ');
+            }
+            System.out.print('\n');
+        }
+
+    }
+
+    /**
+     * Removes the first item from the front of the list.
+     */
+    public Item removeFirst() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            Item nowitem = sentinel.next.item;
+            sentinel.next = sentinel.next.next;
+            sentinel.next.prev = sentinel;
+            size = size - 1;
+            return nowitem;
+        }
+    }
+
+    /**
+     * Removes the first item from the end of the list.
+     */
+    public Item removeLast() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            Item nowitem = sentinel.prev.item;
+            sentinel.prev = sentinel.prev.prev;
+            sentinel.prev.next = sentinel;
+            size = size - 1;
+            return nowitem;
+        }
+    }
+
+    /**
+     * Gets the item at the given index.
+     */
+    public Item get(int index) {
+        if (index >= size) {
+            return null;
+        } else {
+            int i = 0;
+            StuffNode nowget = sentinel;
+            while (i <= index) {
+                nowget = nowget.next;
+                i++;
+            }
+            return nowget.item;
+        }
+    }
+
+
+    private class StuffNode {
+        public StuffNode prev;
+        public Item item;
+        public StuffNode next;
+
+        public StuffNode(Item i, StuffNode p, StuffNode n) {
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+}
--- proj1/deque/LinkedListDequeTest.java
+++ proj1/deque/LinkedListDequeTest.java
@@ -14,8 +14,7 @@ public class LinkedListDequeTest {
      * && is the "and" operation. */
     public void addIsEmptySizeTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+//        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
@@ -25,24 +24,24 @@ public class LinkedListDequeTest {
 		// It's a binary operator that returns true if both arguments true, and false otherwise.
         assertEquals(1, lld1.size());
         assertFalse("lld1 should now contain 1 item", lld1.isEmpty());
+        assertEquals("front", lld1.get(0));
 
 		lld1.addLast("middle");
 		assertEquals(2, lld1.size());
+        assertEquals("middle", lld1.get(1));
 
 		lld1.addLast("back");
 		assertEquals(3, lld1.size());
 
 		System.out.println("Printing out deque: ");
-		lld1.printDeque();
-		*/
+//		lld1.printDeque();
     }
 
     @Test
     /** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
     public void addRemoveTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+//        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty
 		assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
@@ -54,15 +53,13 @@ public class LinkedListDequeTest {
 		lld1.removeFirst();
 		// should be empty
 		assertTrue("lld1 should be empty after removal", lld1.isEmpty());
-        */
     }
 
     @Test
     /* Tests removing from an empty deque */
     public void removeEmptyTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+//        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
         lld1.addFirst(3);
 
@@ -77,14 +74,12 @@ public class LinkedListDequeTest {
         errorMsg += "  actual size() returned 0\n";
 
         assertEquals(errorMsg, 0, size);
-        */
     }
 
     @Test
     /* Check if you can create LinkedListDeques with different parameterized types*/
     public void multipleParamTest() {
 
-        /*
         LinkedListDeque<String>  lld1 = new LinkedListDeque<String>();
         LinkedListDeque<Double>  lld2 = new LinkedListDeque<Double>();
         LinkedListDeque<Boolean> lld3 = new LinkedListDeque<Boolean>();
@@ -94,17 +89,17 @@ public class LinkedListDequeTest {
         lld3.addFirst(true);
 
         String s = lld1.removeFirst();
-        double d = lld2.removeFirst();
-        boolean b = lld3.removeFirst();
-        */
+//        double d = lld2.removeFirst();
+//        boolean b = lld3.removeFirst();
+        Double d = lld2.removeFirst();
+        Boolean b = lld3.removeFirst();
     }
 
     @Test
     /* check if null is return when removing from an empty LinkedListDeque. */
     public void emptyNullReturnTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+//        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 
         boolean passed1 = false;
@@ -112,15 +107,13 @@ public class LinkedListDequeTest {
         assertEquals("Should return null when removeFirst is called on an empty Deque,", null, lld1.removeFirst());
         assertEquals("Should return null when removeLast is called on an empty Deque,", null, lld1.removeLast());
 
-        */
     }
 
     @Test
     /* Add large number of elements to deque; check if order is correct. */
     public void bigLLDequeTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+//        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
         for (int i = 0; i < 1000000; i++) {
             lld1.addLast(i);
@@ -134,6 +127,5 @@ public class LinkedListDequeTest {
             assertEquals("Should have the same value", i, (double) lld1.removeLast(), 0.0);
         }
 
-        */
     }
 }
